987162df842d0bbf1df4f9bd0938d21ef43b4f23
Ming Luo <jiamingluo123@gmail.com>
2020-03-16 20:35:39 -0400

    First wave

---
+++ app/src/main/java/com/example/readmusic/CanvasView.java
@@ -0,0 +1,134 @@
+package com.example.readmusic;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class CanvasView extends View {
+
+    public int width;
+    public int height;
+    private Bitmap mBitmap;
+    private Canvas mCanvas;
+    private Path mPath;
+    Context context;
+    private Paint mPaint;
+    private float mX, mY;
+    private static final float TOLERANCE = 5;
+
+    public CanvasView(Context c, AttributeSet attrs) {
+        super(c, attrs);
+        context = c;
+
+        // we set a new Path
+        mPath = new Path();
+
+        // and we set a new Paint with the desired attributes
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeWidth(4f);
+    }
+
+    // override onSizeChanged
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        // your Canvas will draw onto the defined Bitmap
+        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+    }
+
+    // override onDraw
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        // draw the mPath with the mPaint on the canvas when onDraw
+        canvas.drawPath(mPath, mPaint);
+
+        Paint p = new Paint();
+        p = new Paint();
+        p.setColor(Color.BLACK);
+        p.setStyle(Paint.Style.STROKE);
+        p.setStrokeWidth(3f);
+
+        for (int i = 0; i < 5; i++) {
+            canvas.drawLine(30, 30 + 30 * i, canvas.getWidth() - 30, 30 + 30 * i, p);
+        }
+
+        DrawOvalWithCenter(canvas, 30, 30, 30, 35);
+
+    }
+
+    private void DrawOvalWithCenter(Canvas canvas, int x, int y, int height, int width) {
+        Paint p = new Paint();
+        p = new Paint();
+        p.setColor(Color.BLACK);
+        p.setStyle(Paint.Style.FILL);
+
+        int startX = x - width / 2;
+        int stopX = x + width / 2;
+        int startY = y - height / 2;
+        int stopY = y + height / 2;
+        canvas.drawOval(startX, startY, stopX, stopY, p);
+    }
+    // when ACTION_DOWN start touch according to the x,y values
+    private void startTouch(float x, float y) {
+        mPath.moveTo(x, y);
+        mX = x;
+        mY = y;
+    }
+
+    // when ACTION_MOVE move touch according to the x,y values
+    private void moveTouch(float x, float y) {
+        float dx = Math.abs(x - mX);
+        float dy = Math.abs(y - mY);
+        if (dx >= TOLERANCE || dy >= TOLERANCE) {
+            mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+            mX = x;
+            mY = y;
+        }
+    }
+
+    public void clearCanvas() {
+        mPath.reset();
+        invalidate();
+    }
+
+    // when ACTION_UP stop touch
+    private void upTouch() {
+        mPath.lineTo(mX, mY);
+    }
+
+    //override the onTouchEvent
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                startTouch(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                moveTouch(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                upTouch();
+                invalidate();
+                break;
+        }
+        return true;
+    }
+}
